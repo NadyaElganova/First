2a83eed471f8f52de4e546d93fc82050ca62ccf9
Надежда <elganova-nadya@mail.ru>
2023-10-13 20:40:46 +0400

    Добавлен taghelper EmailLinkTagHelper и ListHelper

---
+++ First/TagHelpers/EmailLinkTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace First.TagHelpers
+{
+    public class EmailLinkTagHelper: TagHelper
+    {
+        public string Email { get; set; }
+        public string MyTitle { get; set; }
+        //    public override void Process(TagHelperContext context, TagHelperOutput output)
+        //    {            
+        //        output.TagName = "button";
+        //        output.Content.Append("Nadya");
+        //    }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            //output.Content.Append("Youtube");
+            //output.Attributes.Add("href", "https://www.youtube.com");
+
+            output.Content.Append(MyTitle ?? Email);
+            output.Attributes.Add("href", $"mailto:{Email}");
+        }
+    }
+}
