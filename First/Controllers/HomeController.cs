17928541f5ee26faa1dd021de809ee4253019fca
Надежда <elganova-nadya@mail.ru>
2023-10-17 23:02:13 +0400

    занятие от 09.10 (пагинация)

--- First/Controllers/HomeController.cs
+++ First/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using First.Models;
 using First.Services;
+using First.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -33,7 +34,7 @@ namespace First.Controllers
 
             return View(movie);
         }
-        public async Task<IActionResult> Search(string title)
+        public async Task<IActionResult> Search(string title, int page=1)
         {
             // search movies
 
@@ -41,20 +42,27 @@ namespace First.Controllers
             //var response = await client.GetAsync($"https://omdbapi.com/?s={title}&apikey=5b8b7798");
             //var result = await  response.Content.ReadAsStringAsync();
             
-            MovieApiResponse result = null;
-
+            SearchViewModel searchViewModel = new SearchViewModel(); 
             try
             {
-                result = await movieApiService.SearchByTitleAsync(title);
+                MovieApiResponse result = await movieApiService.SearchByTitleAsync(title, page);
+                //53   5.3
+
+                searchViewModel.Title = title;
+                searchViewModel.Movies = result.Cinemas;
+                searchViewModel.Response = result.Response;
+                searchViewModel.Error = result.Error;
+                searchViewModel.CurrentPage = page;
+                searchViewModel.TotalPages = (int)Math.Ceiling(result.TotalResults / 10.0);
             }
             catch (Exception ex)
             {
-                ViewBag.errorMessage = ex.Message;
+                searchViewModel.Error = ex.Message;
             }            
 
             //ViewBag.Result = result;
-            ViewBag.searchMovie = title;
-            return View(result);
+            //ViewBag.searchMovie = title;
+            return View(searchViewModel);
         }
 
         public IActionResult Privacy()
